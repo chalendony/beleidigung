ec8bc62a18b8b95a99bc59b09188b6fb08714480
StewartA <a.stewart@eos-did.com>
2021-04-17 05:36:10 +0200

    parse files

--- germeval/readdata.py
+++ germeval/readdata.py
@@ -6,28 +6,23 @@ import pandas as pd
 import re
 
 Germeval = collections.namedtuple('Germeval', 'sentence label_task1 label_task2  label_task3 filename')
-target = ['OFFENSE', 'INSULT', 'EXPLICIT', 'ABUSE', 'OTHER', 'PROFANITY', 'IMPLICIT']
-# TODO create regex from target
-#reg_target = "'OFFENSE | INSULT | EXPLICIT | ABUSE | OTHER | PROFANITY | IMPLICIT'"
-delimeter = 'OFFENSE|OTHER'
 
 def read():
     #tuple
 
     count = 0
-    for name in glob.glob('germeval/EX*.txt'):
+    for name in glob.glob('germeval/*.txt'):
         lst = []
-        print(name)
         with open(name) as fp:
-           
+            print(f" {name}")
             lines = fp.readlines()
             for line in lines:
                 line = line.strip()
                 splits = line.split('\t')
-                print(f" {splits}")
+                #print(f" line: {name} {splits}")
                 sentence = splits[0]
                 labels = splits[1:]
-                print(f" {labels}")
+                #print(f" labels {labels}")
                
                 label_task3 = "NA"
                 if len(labels) == 3:
