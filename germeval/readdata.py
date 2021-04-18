636bf2f9e9b23adaeb90f276a471bcf091017db0
StewartA <a.stewart@eos-did.com>
2021-04-18 18:55:32 +0200

    find exact match in text

--- germeval/readdata.py
+++ germeval/readdata.py
@@ -6,6 +6,84 @@ import pandas as pd
 import re
 
 Germeval = collections.namedtuple('Germeval', 'sentence label_task1 label_task2  label_task3 filename')
+PROFANITY = "PROFANITY"
+OTHER = "OTHER"
+
+def readblist():
+    for name in glob.glob('Beschwerdeliste.csv'):
+        text = []
+        with open(name) as fp:
+            print(f" {name}")
+            lines = fp.readlines()
+            for line in lines:
+                splits = line.split(",")
+                term = splits[1]
+                text.append(term.strip().lower())
+    # for i in text:
+    #     print(f"profanity::  {i}")
+    return text
+
+def exact_match(targetlst):
+    #tuple
+
+    count = 0
+    for name in glob.glob('germeval/*.txt'):
+        text = []
+        with open(name) as fp:
+            print(f" {name}")
+            lines = fp.readlines()
+            for line in lines:
+                line = line.strip()
+                splits = line.split('\t')
+                sentence = splits[0]
+
+                tmp = [(t,line) for t in targetlst if t in line.lower()]
+                if  tmp: 
+                    text.append(tmp)
+# """ 
+#                 labels = splits[1:]
+            
+#                 label_task3 = "NA"
+#                 if len(labels) == 3:
+#                     label_task3 = labels[2] """
+
+                
+   
+    print(f"beschwerde::  {text}")    
+    print(f"Count B-list {len(text)}")   
+    # for i in text:
+    #     print(f"beschwerde::  {i}")
+
+
+def profanity():
+    #tuple
+
+    count = 0
+    for name in glob.glob('germeval/*.txt'):
+        text = []
+        with open(name) as fp:
+            print(f" {name}")
+            lines = fp.readlines()
+            for line in lines:
+                if PROFANITY in line:
+                    line = line.strip()
+                    splits = line.split('\t')
+                    #print(f" line: {name} {splits}")
+                    sentence = splits[0]
+                    labels = splits[1:]
+                    #print(f" labels {labels}")
+                
+                    label_task3 = "NA"
+                    if len(labels) == 3:
+                        label_task3 = labels[2]
+
+                    
+                    text.append(sentence)
+           
+    print(f"Gereval profanity {len(text)}")       
+    for i in text:
+        print(f"profanity::  {i}")
+
 
 def read():
     #tuple
@@ -13,6 +91,7 @@ def read():
     count = 0
     for name in glob.glob('germeval/*.txt'):
         lst = []
+       
         with open(name) as fp:
             print(f" {name}")
             lines = fp.readlines()
@@ -30,13 +109,16 @@ def read():
 
                 lst.append(Germeval(sentence=sentence, label_task1=labels[0], label_task2=labels[1], label_task3=label_task3, 
                     filename=name))
-           
+
     df = pd.DataFrame(lst)
     print(f"shape {df.shape}")
     print(f"shape {df.head}")
 
 
 if __name__ == "__main__":
-    read()
+   #profanity()
+   targets = readblist()
+   intargetlist(targets)
+
 
     
